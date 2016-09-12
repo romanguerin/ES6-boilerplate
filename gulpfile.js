0d9781e94cb508b2345e3732fcb95584e318b286
romanguerin <romanguerin@msn.com>
2016-09-12 10:49:25 +0200

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require ('gulp');
+const babelify = require('babelify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const uglify = require('gulp-uglify');
+//const sourcemaps = require('gulp-sourcemaps')
+
+
+gulp.task('run', function()
+{
+    var bundler = browserify('./src/script.es6',{debug:true})
+
+        .transform(babelify, {presets: ['es2015']})
+        .bundle();
+
+    return bundler
+        .pipe(source('script.js'))
+//        .pipe(sourcemaps.init())
+        .pipe(buffer())
+        .pipe(uglify())
+//        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist'))
+        ;
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./src/**/*.*', ['run']);
+    });
+
+gulp.task('default', ['watch,run']);
+
---
+++ src/script.es6
@@ -0,0 +1,6 @@
+const hoi ="hoi";
+
+var fout = "vout";
+
+fout = vout;
+
